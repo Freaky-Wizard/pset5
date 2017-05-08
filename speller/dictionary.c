d5f1883847aaa1aa2acde91cfa44fa065ea21ae8
Akshay kamboj <akskamboj1998@gmail.com>
2017-05-08 18:41:50 +0000

    initialisation

---
+++ speller/dictionary.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+
+#include <ctype.h>    
+
+#include "dictionary.h"
+
+#include <string.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ int main ()
+ {
+     
+ 
+     return 0;
+ }
+ 
